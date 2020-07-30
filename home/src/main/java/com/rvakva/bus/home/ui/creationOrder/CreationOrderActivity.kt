4bd9e9fa5fe0398db48a41c9e7d91fc57027c978
luochengsc <852942536@qq.com>
2020-07-30 18:17:07 +0800

    代下单部分页面

---
+++ home/src/main/java/com/rvakva/bus/home/ui/creationOrder/CreationOrderActivity.kt
@@ -0,0 +1,10 @@
+package com.rvakva.bus.home.ui.creationOrder
+
+/**
+ * Copyright (C), 2020 - 2999, Sichuan Xiaoka Technology Co., Ltd.
+ * @Description:
+ * @Author: lch
+ * @Date: 2020/7/30 16:55
+ **/
+class CreationOrderActivity {
+}
